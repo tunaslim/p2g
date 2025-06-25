81fafa7eade5eb99d01bfd0218fc188d90818add
tunaslim <tunaslim@gmail.com>
2025-06-25 08:43:21 +0300

    Update route.ts

--- app/api/helm-orders/route.ts
+++ app/api/helm-orders/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: NextRequest) {
 
   try {
     const response = await axios.get(
-      'https://goodlife.myhelm.app/public-api/orders?filters[status][]=3',
+      'https://goodlife.myhelm.app/public-api/orders?filters[status][]=3&filters[status][]=82',
       {
         headers: {
           Authorization: token,
