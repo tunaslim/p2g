6a99197831812063341d7bfad8069f0dcbc86848
tunaslim <tunaslim@gmail.com>
2025-06-27 14:11:37 +0300

    Update page.tsx

--- app/despatch-ready-orders/page.tsx
+++ app/despatch-ready-orders/page.tsx
@@ -424,7 +424,9 @@ export default function DespatchReadyOrders() {
                                 {new Date(q.EstimatedDeliveryDate).toLocaleDateString()}
                                 </div>
                                 <div>
-                                <strong>Max:</strong> {q.Service.MaxWeight}kg<br />
+                                <span className={styles.noWrap}>
+                                <strong>Max:</strong> {q.Service.MaxWeight}kg
+                                </span>
                                 {q.Service.MaxHeight * 100}×{q.Service.MaxWidth * 100}×{q.Service.MaxLength * 100}cm
                                 </div>
                                 </div>
