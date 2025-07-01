6a8aaaf9441a0f43ecda0881503c004109129bb4
tunaslim <tunaslim@gmail.com>
2025-07-01 14:19:59 +0300

    Update page.tsx

--- app/book-order/page.tsx
+++ app/book-order/page.tsx
@@ -2,6 +2,10 @@
 
 import BookOrderPreviewClient from './client/BookOrderPreviewClient';
 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
+
 export default function Page() {
   return <BookOrderPreviewClient />;
 }
