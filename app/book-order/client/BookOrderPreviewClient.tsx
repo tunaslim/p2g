49547597964f2cd67e9052738623e8efcbad6492
tunaslim <tunaslim@gmail.com>
2025-07-01 14:11:29 +0300

    Update BookOrderPreviewClient.tsx

--- app/book-order/client/BookOrderPreviewClient.tsx
+++ app/book-order/client/BookOrderPreviewClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-import styles from './page.module.css';
+import styles from '../page.module.css';
 
 export default function BookOrderPreviewClient() {
   const params = useSearchParams();
