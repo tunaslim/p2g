51fe155fee0c16bae3da2d517330316dc9d42b2c
tunaslim <tunaslim@gmail.com>
2025-07-01 13:41:21 +0300

    Update BookOrderPreviewClient.tsx

--- app/book-order/BookOrderPreviewClient.tsx
+++ app/book-order/BookOrderPreviewClient.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import styles from './page.module.css';
