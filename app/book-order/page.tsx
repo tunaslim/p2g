41414a49568c5b491f4146e9bf42c657fa7b8a65
tunaslim <tunaslim@gmail.com>
2025-07-01 13:48:40 +0300

    Update page.tsx

--- app/book-order/page.tsx
+++ app/book-order/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = 'force-dynamic';
 import BookOrderPreviewClient from './BookOrderPreviewClient';
 export default function Page() {
   return <BookOrderPreviewClient />;
