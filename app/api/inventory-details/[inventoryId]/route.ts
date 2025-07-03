532be73d0bba1c000a684b109fbd687d2fe9e966
tunaslim <tunaslim@gmail.com>
2025-07-03 14:10:20 +0300

    Update route.ts

--- app/api/inventory-details/[inventoryId]/route.ts
+++ app/api/inventory-details/[inventoryId]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, context: { params: { inventoryId: string } }) {
+export async function GET(req: NextRequest, context: any) {
   const inventoryId = context.params.inventoryId;
   const resp = await fetch(`https://goodlife.myhelm.app/public-api/inventory/${inventoryId}`);
   const data = await resp.json();
