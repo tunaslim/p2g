24431a55b7d386bf54e005c251bd71d2c49cdee8
tunaslim <tunaslim@gmail.com>
2025-07-09 15:47:20 +0300

    Update route.ts

--- app/api/helm-login/route.ts
+++ app/api/helm-login/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
 
   try {
-    const response = await axios.post('https://goodlife.myhelm.app/public-api/auth/login', body);
+    const response = await axios.post('https://goodlife.myhelm.app/auth/login', body);
     return NextResponse.json({ token: response.data.token });
   } catch (error: any) {
     return NextResponse.json(
